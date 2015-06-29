6701ba450fbcd9d3cd5e73fde6b7c681143faeee
Muhammad Aliff Muazzam (Tester2009) <alepcat1710@users.noreply.github.com>
2015-06-29 23:12:19 +0800

    Create nmap.lua

---
+++ plugins/nmap.lua
@@ -0,0 +1,19 @@
+do
+ local function run(msg, matches)
+ 	if matches[1] == "specificID" then
+ 	local result = run_command('nmap '..matches[2])
+ 	return 'ID matches !\n\n' ..result
+else
+		return 'ID not matches !'
+	end
+ end
+
+ return {
+ description = "Execute network mapper",
+ usage = "!nmap [COMMAND]",
+ patterns = {
+ "^!nmap (specificID) (.*)$"
+ },
+ run = run,
+ }
+end
